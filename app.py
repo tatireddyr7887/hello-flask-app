d687beaf617734da8278b37ac52248f1b1fdff00
Rajasekhar Reddy <rreddy@arraynetworks.net>
2025-07-10 15:18:20 +0530

    Initial commit for Hello world application

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from AWS!"
+    return "Hello, World from AWS! using GitHub"
 
 if __name__ == "__main__":
     print("Flask app is starting on port 5000...")
