8c6dadb861013307927d832e51d0f914606ebb62
Rajasekhar Reddy <rreddy@arraynetworks.net>
2025-07-11 22:37:14 +0530

    second commit with small change...!

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from AWS! using GitHub"
+    return "Hello, World from AWS! using GitHub Second Time...!"
 
 if __name__ == "__main__":
     print("Flask app is starting on port 5000...")
